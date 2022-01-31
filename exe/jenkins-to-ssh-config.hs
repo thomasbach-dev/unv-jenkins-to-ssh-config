53a3973e38e7f379ae5827e131bac82e20a6648c
Thomas Bach <info@thomasbach.dev>
2022-01-31 04:00:30 +0100

    style: auto-format by stylish-haskell

--- exe/jenkins-to-ssh-config.hs
+++ exe/jenkins-to-ssh-config.hs
@@ -2,16 +2,16 @@ module Main (main) where
 
 import Data.List (intercalate)
 
-import JTSC.Config
 import JTSC
+import JTSC.Config
 
 main :: IO ()
 main = do
   settings <- getSettings
   result <- fetchFromJenkinsAndParse settings
   newConfig <- case result of
-                  Left err -> (error . show) err
-                  Right entries  -> return (intercalate "\n\n" entries ++ "\n\n")
+                  Left err      -> (error . show) err
+                  Right entries -> return (intercalate "\n\n" entries ++ "\n\n")
   case (append settings, sshConfig settings) of
       (False, Just fp) -> writeFile fp newConfig
       (True , Just fp) -> appendFile fp newConfig
